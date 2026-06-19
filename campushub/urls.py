e1f813ad0072911a3aad750fb83cfbb4c3f7da95
Your Name <you@example.com>
2026-06-19 10:19:55 +0530

    Fix urls.py bracket

--- campushub/urls.py
+++ campushub/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('students/', include('students.urls')),
     path('', include('students.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
